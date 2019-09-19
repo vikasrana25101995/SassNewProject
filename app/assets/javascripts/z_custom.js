68510dce2ddea278b026f82b47cbf39ea01b55d2
“ani-03” <ani.anisha03@gmail.com>
2019-09-19 17:10:32 +0530

    Changes

--- app/assets/javascripts/z_custom.js
+++ app/assets/javascripts/z_custom.js
@@ -1,13 +1,27 @@
 $(document).ready(function(){
     bannerSize();
+
+    //Fixed Header
+
+    $(window).scroll(function()
+    {
+      if ($(window).scrollTop() >= '10')
+      {
+        $('#header-bg').addClass('fixed-header');
+      }
+      else
+      {
+        $('#header-bg').removeClass('fixed-header');
+      }
+
+    });
 });
 
 $(window).resize(function(){
     bannerSize();
 });
 
-function bannerSize()
-  {
+function bannerSize(){
     if ($(window).width() <= 767)
     {
       var bannerHeight = $(window).height()-60;
@@ -18,4 +32,4 @@ function bannerSize()
       var bannerHeight = $(window).height()-90;
       $('#carousel .carousel-item').css('height', bannerHeight);
     }
-  }
+}
